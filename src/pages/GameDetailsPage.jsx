c6b1c11b0103fac710969b293cef16c29b75afcb
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-25 18:36:21 +0100

    cleanup for game add function and toast added

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -1,31 +1,44 @@
 import { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { AuthContext } from "../providers/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
 
 const GameDetailsPage = () => {
   const { requestWithToken, userId, isAuthenticated } = useContext(AuthContext);
   const { gameId } = useParams();
   const addGameToAccount = async () => {
-    //add if statement to check if user is authenticated
-    try {
-      const response = await requestWithToken(
-        `/api/users/buygame/${userId}`,
-        "PUT",
-        gameId
-      );
-      if (response.status === 200) {
-        console.log(`game successfully added to your account`);
+    //can only buy game if user is auth
+    if (isAuthenticated) {
+      try {
+        const response = await requestWithToken(
+          `/api/users/buygame/${userId}`,
+          "PUT",
+          gameId
+        );
+        if (response.status === 200) {
+          toast("😎👍 game added to your account", {
+            theme: "dark",
+            autoClose: 3000,
+          });
+          console.log(`game successfully added to your account`);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
+    } else {
+      //replace with actual error handling
+      console.log("please log in");
     }
   };
   return (
     <>
       <h1>GameDetails for game with ID: {gameId}</h1>
       <Link to={`/games/${gameId}/update`}>Update Game</Link>
-      <button type="button">"Buy" Game - Will add game to your account</button>
+      <button type="button" onClick={addGameToAccount}>
+        "Buy" Game - Will add game to your account
+      </button>
       <Link to="/games/1/addreview">Add new review</Link>
+      <ToastContainer />
     </>
   );
 };
