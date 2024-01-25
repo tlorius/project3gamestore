83e6fa50615d065c1f75baaebf983060346b05ed
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-25 09:36:05 +0100

    added toasts to test out functionality

--- src/pages/StorefrontPage.jsx
+++ src/pages/StorefrontPage.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const StorefrontPage = () => {
+  const handleToast = () => toast("Testing :)")
   return (
     <>
       <h1>Homepage</h1>
+      <button onClick={handleToast}>Test Toasts!!</button>
       <div>
         <h3>this is a game</h3>
         <Link to="/games/1">Go to game details of this game</Link>
       </div>
       <Link to="/games/add">Add new game</Link>
+      <ToastContainer />
     </>
   )
 }
