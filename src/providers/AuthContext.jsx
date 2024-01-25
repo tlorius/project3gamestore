7b5e2a988720bec5d509b4541d161f6e346dfb5b
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-25 12:20:00 +0100

    finished basic setup of auth context provider

--- src/providers/AuthContext.jsx
+++ src/providers/AuthContext.jsx
@@ -9,8 +9,83 @@ const AuthContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState()
   const [userId, setUserId] = useState()
 
+  const saveToken = (tokenFromLogin) => {
+    setToken(tokenFromLogin)
+    setIsAuthenticated(true)
+    window.localStorage.setItem("authToken", tokenFromLogin)
+    //check if how userID is stored in the token
+    const { userID } = jwtDecode(tokenFromLogin)
+    setUserId(userId)
+  }
+
+  //might need to handle the cases where a token is expired more gracefully
+  //to redirect user to home or login page
+  const verifyToken = async (tokenFromLocalStorage) => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}`, {
+        headers: { Authorization: `Bearer ${tokenFromLocalStorage}` },
+      })
+      if (response.status === 200) {
+        setIsAuthenticated(true)
+        setToken(tokenFromLocalStorage)
+        setIsLoading(false)
+        const { userId } = jwtDecode(tokenFromLocalStorage)
+        setUserId(userId)
+      } else {
+        setIsLoading(false)
+        window.localStorage.removeItem("authToken")
+      }
+    } catch (error) {
+      console.error(error)
+      setIsLoading(false)
+      window.localStorage.removeItem("authToken")
+    }
+  }
+  //make sure this function actually works
+  const requestWithToken = async (endpoint, method = "GET", payload) => {
+    const url = `${import.meta.env.VITE_API_URL}/api${endpoint}`
+    const options = {
+      method,
+      url,
+      headers: { Authorization: `Bearer ${token}` },
+      data: payload,
+    }
+    try {
+      const response = await axios(options)
+      return response
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const logout = () => {
+    setToken()
+    window.localStorage.removeItem("authToken")
+    setIsAuthenticated(false)
+    setUserId()
+  }
+
+  /* only enable once backend part is done
+ useEffect(() => {
+    const tokenFromLocalStorage = window.localStorage.getItem("authToken")
+    if (tokenFromLocalStorage) {
+      verifyToken(tokenFromLocalStorage)
+    } else {
+      setIsLoading(false)
+    }
+  }, []) */
+
   return (
-    <AuthContext.Provider value={(isAuthenticated, isLoading, useEffect)}>
+    <AuthContext.Provider
+      value={
+        (isAuthenticated,
+        isLoading,
+        saveToken,
+        requestWithToken,
+        logout,
+        userId)
+      }
+    >
       {children}
     </AuthContext.Provider>
   )
