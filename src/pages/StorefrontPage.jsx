228433e1caa7ef91af8890d3339ecdb0394f0051
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-25 17:24:10 +0100

    removed some placeholder code

--- src/pages/StorefrontPage.jsx
+++ src/pages/StorefrontPage.jsx
@@ -38,10 +38,6 @@ const StorefrontPage = () => {
           </Link>
         ))}
       </div>
-      <div>
-        <h3>this is a game</h3>
-        <Link to="/games/1">Go to game details of this game</Link>
-      </div>
 
       <Link to="/games/add">Add new game</Link>
       <button onClick={handleToast}>Test Toasts!!</button>
