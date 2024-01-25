8306bb949990b75323f2ca3566e4a1e98b196298
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-25 17:22:47 +0100

    storefront, gamedetails initialized and linked to gameid

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const GameDetailsPage = () => {
+  const { gameId } = useParams();
   return (
     <>
-      <h1>GameDetails</h1>
-      <Link to="/games/1/update">Update Game</Link>
+      <h1>GameDetails for game with ID: {gameId}</h1>
+      <Link to={`/games/${gameId}/update`}>Update Game</Link>
       <Link to="/games/1/addreview">Add new review</Link>
     </>
   );
--- src/pages/StorefrontPage.jsx
+++ src/pages/StorefrontPage.jsx
@@ -1,21 +1,53 @@
-import { Link } from "react-router-dom"
-import { ToastContainer, toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import classes from "../styles/StorefrontPage.module.css";
+import axios from "axios";
 
 const StorefrontPage = () => {
-  const handleToast = () => toast("Testing 🤣", { theme: "dark" })
+  const [games, setGames] = useState([]);
+
+  const handleToast = () => toast("Testing 🤣", { theme: "dark" });
+
+  const getBooks = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/games`
+      );
+      if (response.status === 200) {
+        setGames(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <>
-      <h1>Homepage</h1>
-      <button onClick={handleToast}>Test Toasts!!</button>
+      <h1>ALL GAMES</h1>
+
+      <div className={classes.gamesCtn}>
+        {games.map((game) => (
+          <Link key={game._id} to={`/games/${game._id}`}>
+            <p>{game.title}</p>
+          </Link>
+        ))}
+      </div>
       <div>
         <h3>this is a game</h3>
         <Link to="/games/1">Go to game details of this game</Link>
       </div>
+
       <Link to="/games/add">Add new game</Link>
+      <button onClick={handleToast}>Test Toasts!!</button>
       <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default StorefrontPage
+export default StorefrontPage;
