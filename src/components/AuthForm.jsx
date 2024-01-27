402d9c2c6ccf1823035195eca3db1d4360cae0d3
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-27 19:21:04 +0100

    final 2fa working

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -2,12 +2,18 @@ import { useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthContext";
+import { Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { toast } from "react-toastify";
 
 const AuthForm = ({ isLogin = false }) => {
   const [loginCredential, setLoginCredential] = useState("");
   const [email, SetEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [opened, { open, close }] = useDisclosure();
+  const [MFACode, setMFACode] = useState("");
+  const [tempToken, setTempToken] = useState("");
 
   const navigate = useNavigate();
 
@@ -34,12 +40,16 @@ const AuthForm = ({ isLogin = false }) => {
         `${import.meta.env.VITE_API_URL}/auth/${isLogin ? "login" : "signup"}`,
         payload
       );
+      if (response.status === 202) {
+        setTempToken(response.data.loginToken);
+        open();
+      }
+
       if (response.status === 201) {
         navigate("/login");
       }
       if (response.status === 200) {
-        //navigate user to page they were trying to access before loggin in if possible
-        console.log(response.data.token);
+        toast.success("Login Successful", { theme: "dark" });
         saveToken(response.data.token);
         navigate("/");
       }
@@ -47,6 +57,26 @@ const AuthForm = ({ isLogin = false }) => {
       console.error(error);
     }
   };
+
+  const handleMFACode = async (event) => {
+    event.preventDefault();
+    const payload = { loginToken: tempToken, twoFactorToken: MFACode };
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/otp/validate`,
+        payload
+      );
+      if (response.status === 200) {
+        toast.success("Login Successful", { theme: "dark" });
+        close();
+        saveToken(response.data.token);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -90,6 +120,26 @@ const AuthForm = ({ isLogin = false }) => {
         </label>
         <input type="submit" value={isLogin ? "Login" : "Sign Up"} />
       </form>
+      <Modal
+        opened={opened}
+        withCloseButton={false}
+        centered
+        closeOnClickOutside={false}
+      >
+        <form onSubmit={handleMFACode}>
+          <label>
+            Please enter your 2FA code from your 2FA App to login:
+            <input
+              type="text"
+              value={MFACode}
+              onChange={(event) => setMFACode(event.target.value)}
+            />
+          </label>
+        </form>
+      </Modal>
+      <button type="button" onClick={open}>
+        test 2fa modal
+      </button>
     </>
   );
 };
