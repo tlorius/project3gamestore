994f73ff64fe773b89c07086e18b68c3d1ac5065
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-27 10:18:05 +0100

    working build: rendering out wishlist

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,5 +1,33 @@
-const WishlistPage = () => {
-  return <h1>Wishlist Page</h1>
-}
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../providers/AuthContext";
+import { UserContext } from "../providers/UserContext";
 
-export default WishlistPage
+const WishlistPage = () => {
+  const { userId, isAuthenticated } = useContext(AuthContext);
+  const { user, setNeedsRefresh } = useContext(UserContext);
+
+  useEffect(() => {
+    setNeedsRefresh();
+  }, [userId]);
+
+  return isAuthenticated ? (
+    <>
+      <h1>Wishlist Page of {user && user.username}</h1>
+      <div>
+        {user.wishlistedGames.map((game) => {
+          return (
+            <div key={game._id}>
+              <h3>{game.title}</h3>
+              <p>{game.description}</p>
+              <p>Price: {game.price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
+
+export default WishlistPage;
