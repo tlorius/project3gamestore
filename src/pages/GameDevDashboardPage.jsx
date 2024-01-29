bb755761d96d171dc33da84f2d12e7f6e4336891
Haris Chughtai <125606755+HChughtai98@users.noreply.github.com>
2024-01-29 12:42:06 +0000

    added dev dashboard but needs work

--- src/pages/GameDevDashboardPage.jsx
+++ src/pages/GameDevDashboardPage.jsx
@@ -1,8 +1,38 @@
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../providers/UserContext";
+
 const GameDevDashboardPage = () => {
+  const [games, setGames] = useState([]);
+  const { userId } = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchGamesByUser = async () => {
+      const response = await fetch(`/api/games/by-user/${userId}`);
+      const data = await response.json();
+      setGames(data);
+    };
+
+    if (userId) {
+      fetchGamesByUser();
+    }
+  }, [userId]);
+
   return (
-    <>
-      <h1>GameDev Dashboard</h1>
-    </>
+    <div>
+      <h1>Game Developer Dashboard</h1>
+      {games.length > 0 ? (
+        <ul>
+          {games.map((game) => (
+            <li key={game.id}>
+              <h2>{game.title}</h2>
+              <p>Description: {game.description}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>You have not created any games yet.</p>
+      )}
+    </div>
   );
 };
 
