e4217e78f5fdf5b0f3a6b61503fae2d60fe2532a
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-30 18:49:40 +0100

    invoices rendered

--- src/pages/UserInvoicesPage.jsx
+++ src/pages/UserInvoicesPage.jsx
@@ -1,15 +1,58 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../providers/AuthContext";
 import { UserContext } from "../providers/UserContext";
+import { toast } from "react-toastify";
+import classes from "../styles/UserInvoicesPage.module.css";
 
 const UserInvoicesPage = () => {
-  const { isAuthenticated } = useContext(AuthContext);
+  const [invoices, setInvoices] = useState();
+  const { isAuthenticated, requestWithToken } = useContext(AuthContext);
   const { user } = useContext(UserContext);
+
+  const fetchInvoices = async () => {
+    try {
+      const response = await requestWithToken(`/invoices/user`);
+      if (response.status === 200) {
+        setInvoices(response.data);
+      }
+    } catch (error) {
+      toast.error("Failed to fetch Invoices");
+    }
+  };
+
+  const formatDate = (dateString) => {
+    let formattedDate = new Date(dateString).toLocaleDateString("en-GB");
+    return formattedDate.replace(/\//g, ".");
+  };
+
+  const openModal = (invoiceId) => {};
+
+  useEffect(() => {
+    fetchInvoices();
+  }, []);
+
   return isAuthenticated ? (
     <>
       <h1>All invoices of a user</h1>
-      <Link to="/invoices/1">actually using a modal so scratch that</Link>
+      {invoices &&
+        invoices.map((invoice) => {
+          return (
+            <div
+              key={invoice._id}
+              className={classes.invoiceCtn}
+              onClick={() => openModal(invoice._id)}
+            >
+              <p>
+                Date: {formatDate(invoice.createdAt)} - Items:{" "}
+                {invoice.fromOrder.items.length} - Total Paid:{" "}
+                {(invoice.fromOrder.totalInEuroCentAfterDiscount / 100).toFixed(
+                  2
+                )}
+                €
+              </p>
+            </div>
+          );
+        })}
     </>
   ) : (
     <>
