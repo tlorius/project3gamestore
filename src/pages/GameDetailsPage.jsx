74d4ca2a6440f55c3d3b707ecdc1b70507e01617
Haris Chughtai <125606755+HChughtai98@users.noreply.github.com>
2024-01-31 16:26:01 +0000

    basic formatting for game detail page

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -32,31 +32,35 @@ const GameDetailsPage = () => {
 
   return game ? (
     <div className={classes.contentCtn}>
-      <Link to={`/games/${gameId}/update`}>Update Game</Link>
+      <div className={classes.header}>
+        <h1 classname={classes.gameTitle}>{game.title}</h1>
+        <Link to={`/games/${gameId}/update`}>Update Game</Link>
+      </div>
       <div className={classes.gameCtn}>
-        <h1>{game.title}</h1>
         <img
           className={classes.gameImage}
           src={game.imageUrl}
           alt={game.title}
         />
-        <p>{game.description}</p>
-        <p>Price: {(game.price / 100).toFixed(2)}€</p>
-        {game.discountInPercent > 0 && (
-          <p>
-            Discount: {game.discountInPercent}% (Save{" "}
-            {((game.price * game.discountInPercent) / 100).toFixed(2)}€)
-          </p>
-        )}
-        <Link to={`/games/${gameId}/reviews`}>
-          Reviews: {game.reviews.length}
-        </Link>
-        {game.reviewScorePercent && (
-          <p>Review Score: {game.reviewScorePercent}%</p>
-        )}
-        <p>Publisher: {game.publisher}</p>
-        <p>Developer: {game.developer}</p>
-        <p>Tags: {game.tags.join(", ")}</p>
+        <div className={classes.gameContent}>
+          <p>{game.description}</p>
+          <p>Price: {(game.price / 100).toFixed(2)}€</p>
+          {game.discountInPercent > 0 && (
+            <p>
+              Discount: {game.discountInPercent}% (Save{" "}
+              {((game.price * game.discountInPercent) / 100).toFixed(2)}€)
+            </p>
+          )}
+          <Link to={`/games/${gameId}/reviews`}>
+            Reviews: {game.reviews.length}
+          </Link>
+          {game.reviewScorePercent && (
+            <p>Review Score: {game.reviewScorePercent}%</p>
+          )}
+          <p>Publisher: {game.publisher}</p>
+          <p>Developer: {game.developer}</p>
+          <p>Tags: {game.tags.join(", ")}</p>
+        </div>
       </div>
       {/* Conditionally render buttons/links depending on user authentication and game ownership */}
       {isAuthenticated && (
