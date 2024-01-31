86fcd2c9829a7ca83ed1af34fbe48d87c3de58e1
Haris Chughtai <125606755+HChughtai98@users.noreply.github.com>
2024-01-31 14:31:33 +0000

    added total/recent revenue

--- src/pages/GameDevDashboardPage.jsx
+++ src/pages/GameDevDashboardPage.jsx
@@ -1,69 +1,57 @@
 import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../providers/AuthContext";
 import { Link } from "react-router-dom";
-import { UserContext } from "../providers/UserContext";
+import { Loader } from "@mantine/core";
 
 const GameDevDashboardPage = () => {
   const [games, setGames] = useState([]);
-  const [revenue, setRevenue] = useState([]);
-  const { requestWithToken, userId } = useContext(AuthContext);
-  const { user } = useContext(UserContext);
+  const [revenue, setRevenue] = useState();
+  const { requestWithToken, userId, isAuthenticated } = useContext(AuthContext);
 
-  const fetchRevenue = async () => {
+  const fetchRevenueData = async () => {
     try {
       const response = await requestWithToken(`/invoices/dev`);
+      console.log("Response from server:", response);
+
       if (response.status === 200) {
-        const revenues = response.data.revenues;
-        if (revenues && revenues.length > 0) {
-          const totalRevenueInEuros = revenues[0].sum;
-          setRevenue(totalRevenueInEuros);
-        }
+        setRevenue(response.data);
+        console.log("Fetched revenues:", response.data);
       }
     } catch (error) {
-      console.error("Error fetching revenue:", error);
+      console.error("Error fetching revenue data:", error);
+    }
+  };
+
+  const fetchGamesByUser = async () => {
+    try {
+      const response = await requestWithToken(`/games/dev/${userId}`);
+      setGames(response.data);
+    } catch (error) {
+      console.error("Error fetching games:", error);
     }
   };
 
   useEffect(() => {
-    const fetchGamesByUser = async () => {
-      const response = await requestWithToken(`/games/dev/${userId}`);
-      setGames(response.data);
-    };
-
     if (userId) {
       fetchGamesByUser();
-      fetchRevenue();
+      fetchRevenueData();
     }
   }, [userId]);
 
-  const handleGamePurchase = async (gameId) => {
-    try {
-      const purchaseResponse = await requestWithToken(
-        `/games/purchase/${gameId}`,
-        "POST"
-      );
-      if (purchaseResponse.status === 200) {
-        fetchRevenue();
-      }
-    } catch (error) {
-      console.error("Error purchasing game:", error);
-    }
-  };
-
-  return (
+  return isAuthenticated ? (
     <div>
       <h1>Game Developer Dashboard</h1>
+      <Link to="/games/add">Add new game</Link>
       {games.length > 0 ? (
         <ul>
           {games.map((game) => (
             <li key={game.id}>
               <Link to={`/games/${game.id}`}>
                 <h2>{game.title}</h2>
-                <p>Price: {game.price}€</p>
+                <p>Price: {(game.price / 100).toFixed(2)}€</p>
                 <p>Description: {game.description}</p>
                 <p>Discount: {game.discountInPercent}%</p>
                 <p>Tags: {game.tags.join(", ")}</p>
-                <h3>Revenue of All Time: {revenue.toFixed(2)}€</h3>
               </Link>
             </li>
           ))}
@@ -71,7 +59,27 @@ const GameDevDashboardPage = () => {
       ) : (
         <p>You have not created any games yet.</p>
       )}
+      <div>
+        {revenue && (
+          <>
+            <h3>
+              Revenue of All Time:{" "}
+              {(revenue?.sumAllTimeByDev.sum / 100).toFixed(2)}€
+            </h3>
+            <h3>All Time Sold Games: {revenue?.sumAllTimeByDev.count}</h3>
+            <h3>
+              Revenue in the Last 30 Days:{" "}
+              {(revenue?.sumThirtyDaysDev.sum / 100).toFixed(2)}€
+            </h3>
+            <h3>Last 30 Days Sold Games: {revenue?.sumAllTimeByDev.count}</h3>
+          </>
+        )}
+      </div>
     </div>
+  ) : (
+    <>
+      <Loader color="blue" size="xl" type="dots" />
+    </>
   );
 };
 
