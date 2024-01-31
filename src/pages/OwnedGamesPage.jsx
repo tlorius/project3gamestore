860a5bb3c071240f68359b7ac34d77116c4984df
michelmnds <m1tchofc@gmail.com>
2024-01-31 15:30:14 +0000

    feat: rendered ownedGames + rendered 'cards' on landingPage

--- src/pages/OwnedGamesPage.jsx
+++ src/pages/OwnedGamesPage.jsx
@@ -1,12 +1,26 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../providers/UserContext";
 
 const OwnedGamesPage = () => {
+  const { user } = useContext(UserContext);
+
+  const userOwnedGames = user.ownedGames;
+  console.log(userOwnedGames);
+
   return (
     <>
       <h1>Shows all games a user purchased</h1>
+
+      {userOwnedGames.map((game) => (
+        <div key={game.id}>
+          <span>{game.title}</span>
+        </div>
+      ))}
+
       <Link to="/games/1/addreview">Add a new review to this game button</Link>
     </>
-  )
-}
+  );
+};
 
-export default OwnedGamesPage
+export default OwnedGamesPage;
