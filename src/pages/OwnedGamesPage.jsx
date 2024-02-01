1208dd2270c97b8a8140deabc59906774e2824ac
michelmnds <m1tchofc@gmail.com>
2024-02-01 14:21:07 +0000

    feat: ownedGamesPage none

--- src/pages/OwnedGamesPage.jsx
+++ src/pages/OwnedGamesPage.jsx
@@ -4,22 +4,52 @@ import { UserContext } from "../providers/UserContext";
 import { AuthContext } from "../providers/AuthContext";
 import { Loader } from "@mantine/core";
 
+import classes from "../styles/OwnedGames.module.css";
+
 const OwnedGamesPage = () => {
   const { user } = useContext(UserContext);
   const { isAuthenticated } = useContext(AuthContext);
 
   return user && isAuthenticated ? (
-    <>
-      <h1>Shows all games a user purchased</h1>
+    <div className={classes.ownedGamesContainer}>
+      <h1 className={classes.ownedGamesTitle}>
+        Games owned by <strong>{user.username}</strong>
+      </h1>
 
       {user.ownedGames.map((game) => (
-        <div key={game._id}>
-          <span>{game.title}</span>
-          <Link to={`/games/${game._id}`}>Store Page</Link>
-          <Link to={`/games/${game._id}/addreview`}>Add Review</Link>
+        <div key={game.id} className={classes.gameCard}>
+          <div
+            className={classes.gameCardImage}
+            style={{ backgroundImage: `url(${game.imageUrl})` }}
+          />
+          <div className={classes.gameCardInfo}>
+            <span className={classes.gameCardTitle}>
+              {game.title.toUpperCase()}
+            </span>
+            <section className={classes.gameCardTags}>
+              {game.tags.map((tag, index) => {
+                return (
+                  <span className={classes.gameCardTag} key={index}>
+                    {tag}
+                  </span>
+                );
+              })}
+            </section>
+            <div className={classes.gameCardPriceContainer}>
+              <Link to={`/games/${game._id}`} className={classes.gameCardBtn}>
+                + Infos
+              </Link>
+              <Link
+                className={classes.wishListButton}
+                to={`/games/${game._id}/addreview`}
+              >
+                Add Review
+              </Link>
+            </div>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   ) : (
     <>
       <Loader color="blue" size="xl" type="dots" />
--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../providers/AuthContext";
 import { UserContext } from "../providers/UserContext";
